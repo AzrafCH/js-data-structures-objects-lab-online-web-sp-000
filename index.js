548c9b5584cdc88077ea40897b4967cfb729e454
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-15 02:07:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Write your solution in this file!
 function updateDriverWithKeyAndValue() {
+  /* this function should take in three arguments: a driver Object, a key and a value */
+  /* This function should not mutate the driver */
+  /*and should return a new driver that has an updated value for the key passed in.*/
+}
 
+function destructivelyUpdateDriverWithKeyAndValue() {
+  /* this function should work the same as updateDriverWithKeyAndValue()
+  but it should mutate the driver parameter passed in */
 }
